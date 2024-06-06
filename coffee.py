223f35e59656c02a0bfb8b83f4dc677610de742d
lyday25 <bilqisalayaki@gmail.com>
2024-06-06 19:27:48 +0100

    modified file

--- coffee.py
+++ coffee.py
@@ -1,7 +1,7 @@
 #create a python logic that will insert user input into an empty coffee array
 coffee=[]
 while True:
-    shopped_item = input("Enter shop items")
+    shopped_item = input("Enter shop items: ")
     if shopped_item == "":
         break
     else:
